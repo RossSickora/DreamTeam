4356ae6a544b993a378c25fde6e152a300fc924a
RossSickora <r.sickora@gmail.com>
2018-04-30 22:12:01 -0500

    stand up Ratingscontroller class for starting point

---
+++ src/main/java/com/team/dream/ratings/service/RatingsController.java
@@ -0,0 +1,6 @@
+package com.team.dream.ratings.service;
+
+public class RatingsController {
+
+    //TODO stand up RatingsController to return a static Team Rating from a Team Rating Repository Given a Teamidentifier
+}
