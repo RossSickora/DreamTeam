3c0dec7d16c0a184e4f6b1218709dadee3481b1f
RossSickora <r.sickora@gmail.com>
2018-04-30 22:09:41 -0500

    adding another model

--- src/main/java/com/team/dream/ratings/models/TeamRating.java
+++ src/main/java/com/team/dream/ratings/models/TeamRating.java
@@ -1,9 +1,25 @@
 package com.team.dream.ratings.models;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class TeamRating {
     private List<MemberRating> memberRatings;
     private Rating rating;
 
+    public void setMemberRatings(ArrayList<MemberRating> memberRatings) {
+        this.memberRatings = memberRatings;
+    }
+
+    public List<MemberRating> getMemberRatings() {
+        return memberRatings;
+    }
+
+    public void setTeamRating(Rating ratingMock) {
+        this.rating = ratingMock;
+    }
+
+    public Rating getRating() {
+        return rating;
+    }
 }
--- src/test/java/com/team/dream/ratings/models/TeamRatingTest.java
+++ src/test/java/com/team/dream/ratings/models/TeamRatingTest.java
@@ -1,8 +1,11 @@
 package com.team.dream.ratings.models;
 
 import org.junit.Before;
+import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+
+import java.util.ArrayList;
 
 import static org.junit.Assert.*;
 
@@ -11,10 +14,30 @@ public class TeamRatingTest {
     TeamRating classUnderTest;
     @Mock
     MemberRating memberRatingMock;
+    @Mock
+    Rating ratingMock;
+
+    ArrayList<MemberRating> memberRatings;
 
     @Before
     public void setUp() throws Exception {
         MockitoAnnotations.initMocks(this);
         classUnderTest = new TeamRating();
+        memberRatings = new ArrayList<>();
+        memberRatings.add(memberRatingMock);
+
+    }
+
+    @Test
+    public void getMemberRatingsReturnsFullListOfMemberRatings(){
+        classUnderTest.setMemberRatings(memberRatings);
+        assertEquals(memberRatings.size(), classUnderTest.getMemberRatings().size());
+       assertEquals(memberRatings, classUnderTest.getMemberRatings());
+    }
+
+    @Test
+    public void getTeamRatingTest(){
+        classUnderTest.setTeamRating(ratingMock);
+        assertEquals(ratingMock, classUnderTest.getRating());
     }
 }
